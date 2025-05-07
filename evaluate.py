230456617a11b243bd375e629299f3ba336bc8ef
Your GitHub Username <kori@rogers.live>
2025-05-07 10:38:35 +0100

    training file complete in full

---
+++ evaluate.py
@@ -0,0 +1,66 @@
+import argparse, torch 
+from pathlib import Path 
+from torch.utils.data import DataLoader 
+from nltk.translate.bleu_score import corpus_bleu 
+from tqdm import tqdm 
+
+from dataset import Flickr30kDataset
+from model import MultiModalCaptioner
+
+
+#greedy decoding 
+def generate(model, pixel, tokenizer, max_len=50, device="cuda"): 
+    model.eval()
+    with torch.no_grad():
+        B = pixel.size(0) #batch size, aka number of images in input 
+        input_ids = torch.full((B, 1), tokenizer.bos_token_id, device=device, dtype=torch.long)
+        
+        for _ in range(max_len):
+            logits, _ = model(pixel, input_ids)
+            next_id = logits[:, -1].argmax(dim=-1, keepdim=True) #select argmax for next token
+            input_ids = torch.cat([input_ids, next_id], dim=1) #concatenate to input
+            
+            if (next_id == tokenizer.eos_token_id).all(): 
+                break 
+        return tokenizer.batch_decode(input_ids[:, 1:], skip_special_tokens=True) #remove bos and decode
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ckpt", type=str, required=False, help="torch .pt checkpoint")
+    args = parser.parse_args()
+    
+    ds = Flickr30kDataset(split="test")
+    dl = DataLoader(ds, batch_size=8, shuffle=False)
+    
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model = MultiModalCaptioner(vocab_size=len(ds.tokenizer)).to(device)
+
+    if args.ckpt: 
+        model.load_state_dict(torch.load(args.ckpt, map_location=device))
+    
+    refs, hyps = [], []
+    for batch in tqdm(dl, desc="Evlauating"):
+        pixel = batch["pixel_values"].to(device)
+        captions = generate(model, pixel, ds.tokenizer, device=device)
+        hyps.extend([[c.split()] for c in captions])
+        refs.extend([[ds.tokenizer.decode(batch["labels"][i], skip_special_tokens=True).split()wha] for i in range(len(captions))])
+        
+    bleu4 = corpus_bleu(refs, hyps)
+    print(f"BLEU score: {bleu4:.3f}")
+    return bleu4
+    
+    
+    
+if __name__ == "__main__":
+    main() 
+    
+    
+#Note to self: because of lack of eos token I would expect our inference to consistently hit max_len, lets print that to check (we could even check the average length of the captions in the dataset and compare to average of what we output)
+
+
+
+#beam search 
+
+
+
+#ok finish the eval script first, then go back and do the train script to add the validation set because we need to go back and add wandb, checkpoint saving, and huggingface anyway. 
--- train_decoder.py
+++ train_decoder.py
@@ -6,8 +6,11 @@ from torch.optim import AdamW
 from torch.nn.functional import cross_entropy 
 from tqdm import tqdm 
 
+from dotenv import load_dotenv 
+import wandb 
+from huggingface_hub import HfApi, hf_hub_upload 
 from dataset import Flickr30kDataset
-from model import MultimodalDecoder
+from model import MultiModalCaptioner 
 
 
 def parse_args(): 
@@ -17,21 +20,37 @@ def parse_args():
     p.add_argument("--epochs", type=int, default=5)
     p.add_argument("--lr", type=float, default=2e-4)
     p.add_argument("--dry_run", action="store_true")
+    p.add_argument("--project", type=str, default="clip-mm-decoder") #wandb project
+    p.add_argument("--repo", type=str, default="clip-mm-decoder") #hf repo 
+    p.add_argument("--save_ckpt", type=str, default=Path("checkpoints"))
     return p.parse_args()
 
 
 def main(): 
+    load_dotenv()
     args = parse_args()
-    ds = Flickr30kDataset(split="train", root=args.root)
-    dl = DataLoader(ds, batch_size = args.batch_size, shuffle=True, num_workers=4, pin_memory=True)
     
+    wandb_mode = "disabled" if args.dry_run else "online"
+    wandb.init(project=args.project, config=vars(args), mode=wandb_mode)
+    
+    hf_api = HfApi(token=os.getenv("HF_TOKEN")) if not args.dry_run else None 
+    run_id = uuid.uuid4().hex[:8]
+    
+    #training set 
+    ds_train = Flickr30kDataset(split="train", root=args.root)
+    dl_train = DataLoader(ds_train, batch_size = args.batch_size, shuffle=True, num_workers=4, pin_memory=True)
+    
+    #validation set 
+    ds_val= Flickr30kDataset(split="val", root=args.root)
+    dl_val = DataLoader(ds_val, batch_size = args.batch_size, shuffle=False, num_workers=4, pin_memory=True)
+    
+    #model and optimiser
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    
-    model = MultiModalCaptioner(vocab_size=len(dset.tokenizer)).to(device)
+    model = MultiModalCaptioner(vocab_size=len(ds_train.tokenizer)).to(device)
     optim = AdamW(filter(lambda p: p.requires_grad, model.parameters()), lr=args.lr) # freeze CLIP 
     
     if args.dry_run: 
-        batch = next(iter(dl))
+        batch = next(iter(dl_train))
         pixel, inp, lbl = (t.to(device) for t in (batch["pixel_values"], batch["input_ids"], batch["labels"]))
         logits, _ = model(pixel, inp) #run forward pass 
         
@@ -40,26 +59,69 @@ def main():
         loss = cross_entropy(
             logits.view(-1, logits.size(-1)), # (B,L,V) -> (B*L, V)
             lbl.view(-1), # (B,L) -> (B*L)
-            ignore_index = ds.tokenizer.pad_token_id, #note: also ignores eos token and unk token 
+            ignore_index = ds_train.tokenizer.pad_token_id, #note: also ignores eos token and unk token 
         )
         print("single pass OK- loss:", loss.item())
         return 
+    
+    args.save_dir.mkdir(parents=True, exist_ok=True)
+    global_step = 0
+
 
     for epoch in range(1, args.epochs + 1): 
+        
+        #training loop 
         model.train()
-        pbar = tqdm(dl, desc=f"Epoch {epoch}")
+        pbar = tqdm(dl_train, desc=f"Epoch {epoch}")
         for batch in pbar:
             pixel, inp, lbl = (t.to(device) for t in (batch["pixel_values"], batch["input_ids"], batch["labels"]))
             logits, _ = model(pixel, inp)
             loss = cross_entropy(
                 logits.view(-1, logits.size(-1)), 
                 lbl.view(-1), 
-                ignore_index = ds.tokenizer.pad_token_id,  #note: also ignores eos token and unk token
+                ignore_index = ds_train.tokenizer.pad_token_id,  #note: also ignores eos token and unk token
             )
             optim.zero_grad()
             loss.backward()
             optim.step()
-            pbar.set_postfix(loss={loss.item(): f"{loss.item():.3f}"})
+            
+            global_step += 1
+            wandb.log({"train/loss": loss.item(), 
+                      "epoch": epoch, 
+                      "step": global_step})
+            pbar.set_postfix(loss=f"{loss.item():.3f}")
+            
+        #validation loop 
+        model.eval()
+        val_loss_sum, n_val_tokens = 0.0, 0 
+        with torch.no_grad():
+            for batch in dl_val:
+                pixel, inp, lbl = (t.to(device) for t in (batch["pixel_values"], batch["input_ids"], batch["labels"]))
+                logits, _ = model(pixel, inp)
+                loss = cross_entropy(
+                    logits.view(-1, logits.size(-1)), 
+                    lbl.view(-1), 
+                    ignore_index = ds_val.tokenizer.pad_token_id, 
+                )
+                val_loss_sum += loss.item()
+                n_val_tokens += (lbl != ds_val.tokenizer.pad_token_id).sum().item()
+        val_loss = val_loss_sum / n_val_tokens 
+        wandb.log({"val/loss": val_loss, "epoch": epoch})
+        print(f"Epoch {epoch} val loss: {val_loss:.3f}")
+        
+        #checkpoint at each epoch, save to wandb a
+        ckpt_path = args.save_dir / f"epoch{epoch}.pt"
+        torch.save(model.state_dict(), ckpt_path)
+        wandb.save(ckpt_path)
+    
+    #save to hf 
+    if args.repo: 
+        hf_api.create_repo(args.repo, exist_ok=True)
+        final_path = args.save_dir / f"model_final_epoch{args.epoch}.pt"
+        shutil.copy(ckpt_path, final_path)
+        hf_hub_upload(repo_id=args.repo, path_or_fileobj=str(final_path), path_in_repo=final_path.name, commit_message=f"Upload model trained for {args.epoch} epochs (run {run_id})")
+        print(f"Model pushed to https://huggingface.co/{args.repo} - file {final_path.name}")
+    wandb.finish()
             
     if __name__ == "__main__": 
         main()
